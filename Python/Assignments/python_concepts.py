a00139eb8c0000bd0742fff29d3e497d6cc7a2fa
Kaden Parker <bobert11@byu.edu>
2025-05-01 14:44:32 -0600

    pushing assignment 1

---
+++ Python/Assignments/python_concepts.py
@@ -0,0 +1,25 @@
+
+# Task 1
+name = "Kaden Parker"
+age = 22
+height = 5.1
+print(f"Hi, my name is {name}, my age is {age}, and my height is {height}")
+
+# Task 2
+numbie1 = 17
+numbie2 = 23.8
+print(f"Sum is: {numbie1 + numbie2}") # Operating on integer var, floar var with standard *-/+ operators
+print(f"Sub is: {numbie1 - numbie2}")
+print(f"Mult is: {numbie1 * numbie2}")
+print(f"Div is: {numbie1 / numbie2}")
+
+# Task 3 
+userval = float(input("Please give me a number: \n"))
+if userval > 0:
+    print("This number is positive. Awesome!")
+if userval < 0:
+    print("This number is negative. Better luck next time!")
+if userval == 0:
+    print("Zero it is. A perfect balance")
+
+
